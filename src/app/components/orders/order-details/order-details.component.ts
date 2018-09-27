9651d7bfbc8d275294f869707e433655351f617c
Robert Melendez <robertm3lendez@gmail.com>
2018-09-26 20:36:18 -0400

    need fixing

--- src/app/components/orders/order-details/order-details.component.ts
+++ src/app/components/orders/order-details/order-details.component.ts
@@ -1,35 +1,37 @@
+import { async } from '@angular/core/testing';
 import { OrderService } from './../../../services/order.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UsersService } from '../../../services/users/users.service';
 import { Order } from '../../../models/order';
+import { map, switchMap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-order-details',
   templateUrl: './order-details.component.html',
   styleUrls: ['./order-details.component.css']
 })
-export class OrderDetailsComponent {
+export class OrderDetailsComponent implements OnInit  {
 
-  id: string;
-  order$;
-  order: Order[] = [];
+  id: string
+  order;
+  orders: Order[] = [];
+  userId: string;
   
 
   constructor(private activedRoute: ActivatedRoute, private orderServ: OrderService, private auth: UsersService) { 
 
-    this.id = this.activedRoute.snapshot.params['id'];
-    this.auth.user.subscribe(user => {
-      if (user) {
-        this.orderServ.getOrderByUser(user.id).subscribe(order => this.order =order);
-        this.order.filter(res => res.items[0].product.id === this.id);
-        
-      }
-    })
+     this.id = this.activedRoute.snapshot.params['id'];
+     this.order =  auth.user.pipe(map(u => u.id));
 
-    
-   
-  }
+    }
+
+   async ngOnInit() {
+
+     let id =  await this.auth.user.pipe(map(u => u.id));
+     console.log(id);
+    }
 
 
 }
