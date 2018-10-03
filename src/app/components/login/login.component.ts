9bb1587a8ce04468738e7fa8d3a3d6fd0c7c068f
Robert <robertm3lendez@gmail.com>
2018-10-03 15:45:05 -0400

    add login feedback

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,14 +7,30 @@ import { LoginsService } from '../../services/logins/logins.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-
+errorMsg = '';
 email: string;
 password: string;
   constructor(private authServ: LoginsService) { }
 
 
  login() {
-    this.authServ.login(this.email, this.password);
+   let login = this.authServ.login(this.email, this.password);
+   console.log(login.then(res => console.log('Res:',res)).catch((err) => {
+     console.log('Error:',err as string);
+     if(err.code === 'auth/argument-error') {
+    
+      this.errorMsg = 'Password and Email required';
+     } else if (err.code === 'auth/wrong-password') {
+       this.errorMsg = 'Invalid password or email does not exist';
+     } else if (err.code === 'auth/too-many-requests') {
+       this.errorMsg = 'Too many unsuccelfull tries. Please try again later.'
+     }
+     else {
+       this.errorMsg = 'User not found.'
+     }
+     
+    }));
+    
     
     }
 
