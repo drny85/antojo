cef8ca8f068e7fc869c85fabfc12e58a59a7c60f
Robert Melendez <robertm3lendez@gmail.com>
2018-10-15 12:41:19 -0400

    add audio to new order

--- src/app/admin/orders/orders.component.ts
+++ src/app/admin/orders/orders.component.ts
@@ -2,8 +2,9 @@ import { Subscription } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { Order } from './../../models/order';
 import { OrderService } from './../../services/order.service';
-import { Component, OnInit, ViewChild, ElementRef, AfterContentChecked, AfterContentInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { count } from 'rxjs/operators';
 
 
 @Component({
@@ -13,12 +14,15 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class OrdersComponent implements OnInit {
 
+  @ViewChild('audioFile') audioFile: ElementRef;
+
   orders$;
   orders: Order[] = [];
   status: string = 'All Orders';
   filteredStatus: Order[] = [];
   subscription: Subscription;
   checkNewOrder: boolean;
+  count: number = 0;
 
   constructor(private orderServ: OrderService, private activeRoute: ActivatedRoute, private msg: ToastrService) { 
     
@@ -27,16 +31,15 @@ export class OrdersComponent implements OnInit {
  async ngOnInit() {
   this.orders$ = (await this.orderServ.getAllOrders());
   this.populateOrders();
-  this.orderServ.newOrderSubmitted.subscribe(order => {this.checkNewOrder = order;
-    console.log(this.checkNewOrder);
-  })
-  
 
   }
 
   private populateOrders() { 
+
+  
    
    this.subscription = this.orderServ.getAllOrders().subscribe(orders => {
+     
       this.orders = orders;
       this.activeRoute.queryParamMap.subscribe(params => {
         this.status = params.get('status');
@@ -44,9 +47,34 @@ export class OrdersComponent implements OnInit {
           this.orders.filter(p => p.status === this.status) :
           this.orders;
       })
+      this.count = 0;
+      orders.filter(order => {
+        
+        if (order.status === 'new') {
+          this.count += 1;
+         
+        }
+        
+      }
      
-    });
-   
+      );
+
+      if (this.count > 0)  {
+        this.msg.show('You have '+ this.count + ' new orders', 'NEW ORDER');
+        console.log(this.audioFile.nativeElement);
+        (<HTMLAudioElement>this.audioFile.nativeElement).play();
+        setTimeout(() =>  (<HTMLAudioElement>this.audioFile.nativeElement).play(), 2100);
+       
+
+      if (this.count < 1)  {
+        this.msg.show('There is no New Orders', 'NO ORDERS');
+      } 
+      
+    }
+
+  })
+    
+    
  }
 
  
